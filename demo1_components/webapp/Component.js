159a6bf0eadc76564110a757a1356e1677610218
AL15103 <caspar.van.tergouw@alliander.com>
2020-06-16 10:45:33 +0200

    extra changes

---
+++ demo1_components/webapp/Component.js
@@ -0,0 +1,20 @@
+/* global mobx */
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"ComponentBasedExampleProject/mockData/mockData"
+], function(UIComponent, JSONModel, mockData) {
+	"use strict";
+	return UIComponent.extend("ComponentBasedExampleProject.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function() {
+			var oModel = new JSONModel(mockData);
+			this.setModel(oModel);
+
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+		},
+	});
+});
